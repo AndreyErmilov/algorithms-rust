38048cd29e7fa163ba9d5a288d76a1e8ad2065f9
andreyermilov <andrerm@ya.ru>
2019-11-10 18:39:45 +0300

    Inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Algorithms-Rust
+
+Implementation of algorithms in Rust for educational purposes.
+
+### [Sort Algorithms](./src/sorting)
+
+- [Insertion](./src/sorting/insertion.rs)
---
+++ src/sorting/insertion.rs
@@ -0,0 +1,38 @@
+#[allow(dead_code)]
+fn insertion_sort<T>(mut values: Vec<T>) -> Vec<T>
+where T: Ord {
+    for i in 1..values.len() {
+        for j in (0..i).rev() {
+            if values[j] > values[j + 1] {
+                values.swap(j, j + 1);
+            } else {
+                break
+            };
+        }
+    };
+    values
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let arr: Vec<i32> = Vec::new();
+        assert_eq!(insertion_sort(arr), vec![]);
+    }
+
+    #[test]
+    fn numbers() {
+        let arr = vec![5, 12, -1, 15, 10, 1, 3, 0];
+        assert_eq!(insertion_sort(arr), vec![-1, 0, 1, 3, 5, 10, 12, 15]);
+    }
+
+    #[test]
+    fn chars() {
+        let arr = vec!["b", "a", "c"];
+        assert_eq!(insertion_sort(arr), vec!["a", "b", "c"]);
+    }
+
+}
